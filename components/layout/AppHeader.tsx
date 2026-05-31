f44880230a372325655c97e4211c7e24207243b6
Tahnoon <Tahnoons.v@gmail.com>
2026-05-31 15:07:41 +0400

    style: add dynamic logo to top navigation bar

--- components/layout/AppHeader.tsx
+++ components/layout/AppHeader.tsx
@@ -1,10 +1,20 @@
+import Image from "next/image";
+
 interface AppHeaderProps {
   children: React.ReactNode;
 }
 
 export function AppHeader({ children }: AppHeaderProps) {
   return (
-    <header className="flex h-16 shrink-0 items-center justify-end bg-white px-6 dark:bg-black">
+    <header className="flex h-16 w-full shrink-0 items-center justify-between bg-white px-4 dark:bg-black">
+      <Image
+        src="/logo.png"
+        alt="Sikka Logo"
+        width={100}
+        height={40}
+        priority
+        className="h-auto w-24 transition-all duration-300 dark:invert"
+      />
       {children}
     </header>
   );
